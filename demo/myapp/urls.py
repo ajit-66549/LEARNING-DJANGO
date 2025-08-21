db6e960430f703fca757568359c349d2bc6ee144
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-08-21 15:17:40 -0500

    Changed the url route from nothing to home

--- demo/myapp/urls.py
+++ demo/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("temp/", views.temp, name="temp"),
     path("todos/", views.todos, name="todos")
 ]
